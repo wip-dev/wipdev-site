3d5ddf604085c0cbcbfc9e7c4a62d63492e70d51
wip-dev <wipdev.tech@gmail.com>
2023-08-13 16:46:01 +0300

    feat: initial markdown parser test

--- src/blog/ssg.clj
+++ src/blog/ssg.clj
@@ -1,2 +1,12 @@
-(ns blog.ssg)
+(ns blog.ssg
+  (:require [markdown.core :as md]))
 
+(slurp (clojure.java.io/file "src/blog/posts/what-got-me-here.md"))
+
+(md/md-to-html
+ "src/blog/posts/what-got-me-here.md"
+ "public/posts/what-got-me-here.html")
+
+(defn -main
+  []
+  (println markdown.core/md-to-html))
