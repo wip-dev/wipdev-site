70f4ab30738a611cb52dcb3000e4f2346b31c150
wip-dev <wipdev.tech@gmail.com>
2023-08-14 15:04:34 +0300

    refactoring with docstrings

--- src/clojure/ssg/main.clj
+++ src/clojure/ssg/main.clj
@@ -5,15 +5,23 @@
             [markdown.core :as md]
             [hiccup2.core :as h]))
 
-(def md->hiccup (comp h/raw md/md-to-html-string))
+(defn md->hiccup
+  "Given a Markdown string,
+  return a raw HTML object for insertion in Hiccup."
+  [md-string]
+  (h/raw (md/md-to-html-string md-string)))
 
 (defn post-html-target
+  "Given a Markdown file (java.io.File),
+  return the target HTML file path."
   [md-file]
   (-> (.getPath md-file)
       (#(s/replace % #"src/content" "public"))
       (#(s/replace % #".md" ".html"))))
 
 (defn wrap-post
+  "Given a raw HTML object (using hiccup2.core/raw),
+  wrap it in an HTML skeleton and return as a string."
   [post-html]
   (str "<!DOCTYPE html>"
        (h/html
@@ -25,17 +33,25 @@
            post-html]]])))
 
 (defn make-content
-  [file]
-  {:target (post-html-target file)
-   :content ((comp wrap-post md->hiccup slurp) file)})
+  "Given a Markdown file (java.io.File),
+  return a map with the target HTML file path and content."
+  [md-file]
+  {:target (post-html-target md-file)
+   :content ((comp wrap-post md->hiccup slurp) md-file)})
+
+(defn filter-files
+  "Given a file-seq, filter files and not directories."
+  [f-seq]
+  (filter #(not (.isDirectory %)) f-seq))
 
 (defn -main
   []
-  (let [files    (filter #(not (.isDirectory %))
-                         (file-seq (file "src/content/")))
-        contents (map make-content files)]
+  (let [contents (-> (file "src/content/")
+                     file-seq
+                     filter-files
+                     (#(map make-content %)))]
     (sh "find" "public/" "-name" "*.html" "-type" "f" "-delete")
-    (println "Removed HTML files in `public/`")
+    (println "Removed HTML files in public/")
     (doseq [{target :target content :content} contents]
       (spit target content)
       (println "Wrote " target))))
